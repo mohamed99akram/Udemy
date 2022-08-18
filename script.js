7053b4ee49246148748f9d4a2ace0c4356336bde
mohamed99akram <mohamed1999akram@gmail.com>
2022-08-18 23:49:57 +0200

    added tabs but not tested

--- script.js
+++ script.js
@@ -21,15 +21,46 @@ function createCourse(course) {
             `;
     return html;
 }
-
+let currentTab = "Python";
+function updateCourses(courses){
+    
+    let coursesContainer = document.querySelector(".courses-container");
+    coursesContainer.innerHTML = "";
+    courses.forEach((course) => {
+        if(course.category == currentTab){
+            coursesContainer.innerHTML += createCourse(course);
+        }
+    });
+}
 fetch(
   "https://raw.githubusercontent.com/mohamed99akram/Udemy/phase2/courses.json"
 )
   .then((response) => response.json())
   .then((json) => {
-    let coursesContainer = document.querySelector(".courses-container");
     let courses = json.courses;
-    courses.forEach((course) => {
-        coursesContainer.innerHTML += createCourse(course);
-    });
+    if(courses){
+        localStorage.setItem("courses", JSON.stringify(courses));
+        localStorage.setItem("marketing", JSON.stringify(json.marketing));
+    }
+    updateCourses(courses);
+
   });
+
+let coursesTabs = document.querySelectorAll(".tablinks");
+coursesTabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+        // make all unactive
+        coursesTabs.forEach((tab2)=>{tab2.classList.remove('active')});
+        // make current active
+        tab.classList.add('active');
+        // change current tab's courses
+        currentTab = tab.innerText;
+        let courses = JSON.parse(localStorage.getItem("courses"));
+        updateCourses(courses);
+        // change marketing
+        let marketing = JSON.parse(localStorage.getItem("marketing"));
+        let useless = document.querySelector('.useless');
+        useless.querySelector('h2').innerText = marketing[currentTab].brief;
+        useless.querySelector('p').innerText = marketing[currentTab].details;
+    });
+});
