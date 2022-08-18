3c0ba3959057dd0c87237e0085e1210cb32daf9f
mohamed99akram <mohamed1999akram@gmail.com>
2022-08-18 18:45:56 +0200

    fetching data and displaying it at start

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function getJson(){
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+function createCourse(course) {
+    let html = 
+        `
+            <div class="course">
+                <a href="#">
+                    <img src="${course?.image??"assets/PLMP"}" alt="courseImg" width="100%">
+                    <div class="course-text">
+                        <h3>${course?.title??"Course Name"}</h3><span class="instructorName">${course?.instructor??"Jeff Bezos"}</span>
+                        <div>
+                            <span class="rating">${course?.rating??4.5}</span>
+                            ${course.rating>=1?`<span class="fa fa-star checked"></span>`:`<span class="fa fa-star"></span>`}
+                            ${course.rating>=2?`<span class="fa fa-star checked"></span>`:`<span class="fa fa-star"></span>`}
+                            ${course.rating>=3?`<span class="fa fa-star checked"></span>`:`<span class="fa fa-star"></span>`}
+                            ${course.rating>=4?`<span class="fa fa-star checked"></span>`:`<span class="fa fa-star"></span>`}
+                            ${course.rating>=5?`<span class="fa fa-star checked"></span>`:`<span class="fa fa-star"></span>`}
+                        </div>
+                        <h3>${course?.price??"Free"}</h3>
+                    </div>
+                </a>
+            </div>
+            `;
+    return html;
 }
-getJson();
+
+fetch(
+  "https://raw.githubusercontent.com/mohamed99akram/Udemy/phase2/courses.json"
+)
+  .then((response) => response.json())
+  .then((json) => {
+    let coursesContainer = document.querySelector(".courses-container");
+    let courses = json.courses;
+    courses.forEach((course) => {
+        coursesContainer.innerHTML += createCourse(course);
+    });
+  });
